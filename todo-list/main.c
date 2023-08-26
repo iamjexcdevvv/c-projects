4023f3eb6e69274ab9400d7134736acb5054f9d0
JexcDeveloper <dublasandrei@gmail.com>
2023-08-26 16:04:42 +0800

    adding task reduced time complexity

--- todo-list/main.c
+++ todo-list/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 struct node
 {
@@ -8,38 +9,37 @@ struct node
     struct node *link;
 };
 
-void todo_add_node(struct node *head, struct node *new_node)
+void todo_remove_node(struct node **head, int position_to_remove)
 {
-    if (head == NULL)
+    if (*head == NULL)
     {
         printf("Task list is empty!\n");
-        exit(EXIT_SUCCESS);
     }
-
-    struct node *node_ptr = head;
-
-    while (node_ptr->link != NULL)
+    else if ((*head)->link == NULL)
     {
-        node_ptr = node_ptr->link;
+        free(*head);
+        *head = NULL;
     }
-
-    node_ptr->link = new_node;
+    else
+    {
+        // TODO - Remove specific task
+    }
 }
 
-void todo_create_node(char task[], struct node *head, int current_line)
+void todo_add_node(char task[], struct node **tail, struct node *head)
 {
     struct node *new_node = (struct node *)malloc(sizeof(struct node));
     strcpy(new_node->data, task);
-    new_node->link = NULL;
 
-    if (current_line == 2)
+    if (head->link == NULL)
     {
         head->link = new_node;
     }
     else
     {
-        todo_add_node(head, new_node);
+        (*tail)->link = new_node;
     }
+    *tail = new_node;
 }
 
 void todo_save(char task[])
@@ -53,21 +53,24 @@ void todo_save(char task[])
 void todo_task_display(struct node *head)
 {
     struct node *node_ptr = head;
+    int position = 1;
 
     printf("Task List:\n");
 
     while (node_ptr != NULL)
     {
-        printf("%s\n", node_ptr->data);
+        printf("%d.%s\n", position, node_ptr->data);
         node_ptr = node_ptr->link;
+        position++;
     }
 }
 
-void todo_load_task(FILE *file)
+struct node *todo_load_task(FILE *file)
 {
     char task[32 + 1];
 
     struct node *head = (struct node *)malloc(sizeof(struct node));
+    struct node *tail = NULL;
 
     int current_line = 1;
 
@@ -87,17 +90,23 @@ void todo_load_task(FILE *file)
         }
         else
         {
-            todo_create_node(task, head, current_line);
+            todo_add_node(task, &tail, head);
         }
         current_line++;
     }
 
     todo_task_display(head);
+
+    return head;
 }
 
 void get_user_input(int operation)
 {
-    if (operation == 1)
+    if (operation == 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else if (operation == 1)
     {
         char task[32 + 1];
         fgets(task, sizeof(task), stdin);
@@ -113,7 +122,31 @@ void get_user_input(int operation)
             exit(EXIT_FAILURE);
         }
 
-        todo_load_task(fptr);
+        struct node *head = todo_load_task(fptr);
+        todo_task_display(head);
+        fclose(fptr);
+    }
+    else if (operation == 3)
+    {
+        FILE *fptr = fopen("D:/DSA/projects/todo-list/database/todo-list.txt", "r");
+
+        if (fptr == NULL)
+        {
+            printf("The specified file cannot be read!\n");
+            exit(EXIT_FAILURE);
+        }
+
+        int position;
+
+        struct node *head = todo_load_task(fptr);
+        todo_task_display(head);
+
+        printf("Please enter the position of a task you want to remove:\t");
+        scanf(" %d", &position);
+
+        todo_remove_node(&head, position);
+        // todo_task_display(head);
+
         fclose(fptr);
     }
     else
@@ -126,12 +159,17 @@ void get_user_input(int operation)
 void init_app()
 {
     int operation;
-    printf("Enter 1 To Add Task\n");
-    printf("Enter 2 To View Tasks\n");
-    printf("Please enter a number:\t");
-    scanf("%d", &operation);
-    getchar(); // This will consume the new line that produced by scanf function so later on we can use fgets function
-    get_user_input(operation);
+    while (true)
+    {
+        printf("Enter 0 To end the program\n");
+        printf("Enter 1 To Add Task\n");
+        printf("Enter 2 To View Tasks\n");
+        printf("Enter 3 To Delete a task\n");
+        printf("Please enter a number:\t");
+        scanf("%d", &operation);
+        getchar(); // This will consume the new line that produced by scanf function so later on we can use fgets function
+        get_user_input(operation);
+    }
 }
 
 int main()
